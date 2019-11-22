39ba04cecf499c2ec5f31410b0798c200a85229a
Alex Munteanu <alexandru.munteanu@e-uvt.ro>
2019-11-22 15:40:03 +0200

    On-the-fly GTI creation for multiple-geojsons.
    Modify tests configuration to work with the same loader for multi-geojsons.

--- src/hugin/preprocessing/rasterize.py
+++ src/hugin/preprocessing/rasterize.py
@@ -6,39 +6,56 @@ import rasterio.features
 from hugin.engine.core import RasterGenerator
 
 
-class GroundTruthComponentGenerator(RasterGenerator):
+class ComponentGenerator(RasterGenerator):
+    def __init__(self, base_component):
+        """
+        Args:
+            base_component: the component we should use for detecting the size, etc
+        """
+        self._base_component = base_component
+
+    def _create_component(self, scene_components):
+        raise NotImplementedError  # this should not be implemented here
+
+    def __call__(self):
+        raise NotImplementedError  # this should not be implemented here
+
+
+class RasterFromShapesGenerator(ComponentGenerator):
     """
-    Generator generating ground truth components using shapefiles/geojsons
+    Generator creating ground truth components from ESRI shapefiles or geojsons
     """
     def __init__(self, base_component, shape_input, growth_factor=0, default_value=1, all_touched=True):
         """
         Args:
-            base_component: the component we should use for detecting the size, etc
             shape_input: path of a geojson or shapefile whose geometries we burn in the GTI \
-            or an existing component type's name from the FileSystemLoader
+            or an existing component name
             growth_factor: how much we want to increase the width of the geometries
             default_value: value to write in raster where geometries overlap
             all_touched: If True, all pixels touched by geometries will be burned in. \
             If false, only pixels whose center is within the polygon will be burned in.
         """
-        self._base_component = base_component
+        super().__init__(base_component)
         self._shape_input = shape_input
         self._growth_factor = growth_factor
         self._default_value = default_value
         self._all_touched = all_touched
 
-    def _create_gti(self, scene_components):
-        # this means shape_input is a component in our scene_components already found by FSLoader
+    def _create_component(self, scene_components):
         if self._shape_input in scene_components.keys():
-            self._shape_input = scene_components[self._shape_input]
+            _shape_input = scene_components[self._shape_input]
+        else:
+            _shape_input = gpd.read_file(self._shape_input)
 
-        with rio.open(scene_components[self._base_component]) as base_raster:
-            gt_component = gpd.read_file(self._shape_input)
-            shapes = gt_component['geometry']
-            gti_component = np.zeros(base_raster.shape, dtype=np.uint16)
-            profile = base_raster.profile
-            profile.update(count=1, nodata=0)
+        base_raster = scene_components[self._base_component]
+        gt_component = _shape_input
 
+        shapes = gt_component['geometry']
+        gti_component = np.zeros(base_raster.shape, dtype=np.uint16)
+        profile = base_raster.profile
+        profile.update(count=1, nodata=0)
+
+        if not shapes.empty:
             if self._growth_factor != 0:
                 shapes = shapes.buffer(self._growth_factor)
 
@@ -49,12 +66,7 @@ class GroundTruthComponentGenerator(RasterGenerator):
                                         transform=base_raster.transform,
                                         out=gti_component)
 
-            # works for the first one:
-            fname = scene_components[self._base_component].split('.')[0] + "_gti.tif"
-            with rasterio.open(fname, 'w', **profile) as dst:
-                dst.write(gti_component.astype(np.uint8), 1)
-
-            return rio.io.MemoryFile(bytes(gti_component))
+        return rio.io.MemoryFile(bytes(gti_component))
 
     def __call__(self, scene_components):
-        return self._create_gti(scene_components)
+        return self._create_component(scene_components)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,125 +1,145 @@
-import pytest
-
-import os
 import json
+import os
 import random
+from tempfile import TemporaryDirectory
+
 import numpy as np
+import pytest
 import rasterio
 import rasterio.features
 import rasterio.warp
-
-from tempfile import TemporaryDirectory
-from PIL import Image, ImageDraw
 from rasterio.transform import from_origin
+from skimage.draw import rectangle, circle
+
 from hugin.io import FileSystemLoader
+from hugin.preprocessing.rasterize import RasterFromShapesGenerator
 
 
-def generate_filesystem_loader(width=2131, height=1979, size=35, num_images=10):
-    tempdir = TemporaryDirectory("-hugin")
-    tempdir_name = tempdir.name
-    random.seed(42)
+class GenerateFileSystemLoader():
+    __loader = None
 
-    match_color = "red"
-    coords = (21.2086, 45.7488)
+    def __init__(self, multi_json=True):
+        self._multi_json = multi_json
+        pass
 
-    for imgidx in range(0, num_images):
-        data = np.zeros((height, width, 3), dtype=np.uint8)
-        data_mask = np.zeros((height, width), dtype=np.uint8)
-        im = Image.fromarray(data)
-        mask = Image.fromarray(data_mask, mode="L")
+    def __call__(self, width=2131, height=1979, size=35, num_images=10):
+        if self.__loader is None:
+            self.__loader = self._generate_filesystem_loader(width, height, size, num_images)
+        return self.__loader
 
-        draw = ImageDraw.Draw(im)
-        mdraw = ImageDraw.Draw(mask)
-        colors = ["red", "blue", "green", "white"]
+    def _generate_filesystem_loader(self, width=2131, height=1979, size=35, num_images=10):
+        tempdir = TemporaryDirectory("-hugin")
+        tempdir_name = tempdir.name
+        random.seed(42)
+        coords = (21.2086, 45.7488)
         num_rect = 5
         num_circle = 4
+        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (128, 128, 128)]
+        match_color = colors[0]
 
-        for i in range(0, num_rect):
-            startx = random.randint(0, width-50)
-            starty = random.randint(0, height-50)
-            endx = random.randint(startx+10, startx+random.randint(startx+10, startx+size))
-            endy = random.randint(starty+10, starty+random.randint(starty+10, starty+size))
-            color = random.choice(colors)
-            draw.rectangle([(startx, starty), (endx, endy)], fill=color)
-            if match_color == color:
-                mdraw.rectangle([(startx, starty), (endx, endy)], fill=color)
+        for imgidx in range(0, num_images):
+            data = np.zeros((height, width, 3), dtype=np.uint8)
+            data_mask = np.zeros((height, width), dtype=np.uint8)
 
-        for i in range(0, num_circle):
-            startx = random.randint(0, width-50)
-            starty = random.randint(0, height-50)
-            endx = random.randint(startx+20, startx+random.randint(startx+10, startx+size))
-            endy = random.randint(starty+20, starty+random.randint(starty+10, starty+size))
-            color = random.choice(colors[:-1])
-            draw.ellipse([(startx, starty), (endx, endy)], fill=color)
-            if match_color == color:
-                mdraw.ellipse([(startx, starty), (endx, endy)], fill=color)
+            for i in range(0, num_rect):
+                color = random.choice(colors)
+                startx = random.randint(1, width - size - 5)
+                starty = random.randint(1, height - size - 5)
 
-        fname = os.path.join(tempdir_name, "TEST_RGB_{}.tiff".format(imgidx+1))
-        mname = os.path.join(tempdir_name, "TEST_GTI_{}.tiff".format(imgidx+1))
-        gname = os.path.join(tempdir_name, "TEST_GT_{}.geojson".format(imgidx+1))
+                endx = startx + size
+                endy = starty + size
+                rr, cc = rectangle((startx, endx), (starty, endy), shape=data_mask.shape)
+                data[rr, cc] = color
+                if color == match_color:
+                    data_mask[rr, cc] = 1
 
-        res = 0.5
-        transform = from_origin(coords[0], coords[1], res, res)
-        coords = (coords[0] + (res * width), coords[1])
-        rgb_image = rasterio.open(
-            fname,
-            'w',
-            height=data.shape[0],
-            width=data.shape[1],
-            count=3,
-            driver='GTiff',
-            dtype=data.dtype,
-            transform=transform,
-            crs={'init': 'epsg:3857'},
-        )
-        gti_image = rasterio.open(
-            mname,
-            'w',
-            height=data_mask.shape[0],
-            width=data_mask.shape[1],
-            count=1,
-            driver='GTiff',
-            dtype=data_mask.dtype,
-            transform=transform,
-            compress='none',
-            tiled=True,
-            crs={'init': 'epsg:3857'},
-        )
-        data = np.array(im.getdata()).reshape(data.shape).astype(np.uint8)
-        new_data_mask = np.array(mask.getdata())
-        data_mask = new_data_mask.reshape(data_mask.shape)
-        data_mask = data_mask > 0
-        data_mask = data_mask.astype(np.uint8)
-        for i in range(0, data.shape[-1]):
-            rgb_image.write(data[:,:,i], i+1)
-        gti_image.write(data_mask, 1)
+            for i in range(0, num_circle):
+                color = random.choice(colors)
+                startx = random.randint(1, width - size - 5)
+                starty = random.randint(1, height - size - 5)
+                radius = random.randint(1, 2 * size)
+                rr, cc = circle(startx, starty, radius, shape=data_mask.shape)
+                data[rr, cc] = color
+                if color == match_color:
+                    data_mask[rr, cc] = 255
 
-        gti_image.close()
-        rgb_image.close()
+            fname = os.path.join(tempdir_name, "TEST_RGB_{}.tiff".format(imgidx + 1))
+            mname = os.path.join(tempdir_name, "TEST_GTI_{}.tiff".format(imgidx + 1))
+            gname = os.path.join(tempdir_name, "TEST_GT_{}.geojson".format(imgidx + 1))
+            sgname = os.path.join(tempdir_name, "single_GT.geojson")
 
-        with rasterio.open(mname) as gti:
-            src = gti.read()
-            mask = src == 1
-            features_col = {'type': 'FeatureCollection', 'features': []}
-            for geom, _ in rasterio.features.shapes(src, mask=mask, transform=transform):
-                features_col['features'].append({'geometry': geom})
+            res = 0.5
+            transform = from_origin(coords[0], coords[1], res, res)
+            coords = (coords[0] + (res * width), coords[1])
+            rgb_image = rasterio.open(
+                fname,
+                'w',
+                height=data.shape[0],
+                width=data.shape[1],
+                count=3,
+                driver='GTiff',
+                dtype=data.dtype,
+                transform=transform,
+                crs={'init': 'epsg:3857'},
+            )
+            gti_image = rasterio.open(
+                mname,
+                'w',
+                height=data_mask.shape[0],
+                width=data_mask.shape[1],
+                count=1,
+                driver='GTiff',
+                dtype=data_mask.dtype,
+                transform=transform,
+                compress='none',
+                tiled=True,
+                crs={'init': 'epsg:3857'},
+            )
 
-            with open(gname, 'w') as dst:
-                dst.write(json.dumps(features_col))
+            for i in range(0, data.shape[-1]):
+                rgb_image.write(data[:, :, i], i + 1)
+            gti_image.write(data_mask, 1)
 
-    base_kwargs = {
-        'data_pattern': r"(?P<name>[0-9A-Za-z]+)_(?P<SECOND>[A-Za-z0-9]+)_(?P<THIRD>[A-Za-z0-9]+)\.(tiff|geojson)$",
-        'id_format': "{name}_{THIRD}",
-        'type_format': "{SECOND}",
-        'input_source': tempdir_name,
-        'validation_percent': 0.2
-    }
+            gti_image.close()
+            rgb_image.close()
 
-    loader = FileSystemLoader(**base_kwargs)
-    loader.__temp_input_directory = tempdir
+            with rasterio.open(mname) as gti:
+                src = gti.read()
+                mask = src == 255
+                features_col = {'type': 'FeatureCollection', 'features': []}
+                for geom, _ in rasterio.features.shapes(src, mask=mask, transform=transform):
+                    features_col['features'].append({'geometry': geom})
 
-    return loader
+                    with open(sgname, 'a') as dst_single_geojson:
+                        dst_single_geojson.write(json.dumps(features_col))
+
+                with open(gname, 'w') as dst_geojson:
+                    dst_geojson.write(json.dumps(features_col))
+
+        base_kwargs = {
+            'data_pattern': r"(?P<name>[0-9A-Za-z]+)_(?P<SECOND>[A-Za-z0-9]+)_(?P<THIRD>[A-Za-z0-9]+)\.(tiff|geojson)$",
+            'id_format': "{name}_{THIRD}",
+            'type_format': "{SECOND}",
+            'input_source': tempdir_name,
+            'validation_percent': 0.2,
+        }
+
+        if self._multi_json == True:
+            base_kwargs['dynamic_types'] = {'GENERATED_GROUNDTRUTH': RasterFromShapesGenerator(base_component='RGB',
+                                                                                               shape_input='GT',
+                                                                                               growth_factor=0.00002)}
+        else:
+            base_kwargs['dynamic_types'] = {'GENERATED_GROUNDTRUTH' : RasterFromShapesGenerator(base_component='RGB',
+                                                                                                shape_input=sgname,
+                                                                                                growth_factor=0.00002)}
+
+        loader = FileSystemLoader(**base_kwargs)
+        loader.__temp_input_directory = tempdir
+
+        return loader
+
 
 @pytest.fixture
-def generated_filesystem_loader():
-    return generate_filesystem_loader(num_images=10)
+def generated_filesystem_loader(multiple_json=True):
+    loader = GenerateFileSystemLoader(multiple_json)
+    return loader()
