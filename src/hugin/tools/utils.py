8634d5e385fffa981fc50187fc805550ecc180f4
Marian Neagul <marian@info.uvt.ro>
2020-09-06 22:11:52 +0300

    added patched MeanIoU

--- src/hugin/tools/utils.py
+++ src/hugin/tools/utils.py
@@ -95,7 +95,11 @@ def custom_objects(objects):
     return __wrap
 
 
-
+class MeanIoU(tf.keras.metrics.MeanIoU):
+    def __call__(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.argmax(y_pred, axis=-1)
+        return super().__call__(y_true, y_pred, sample_weight=sample_weight)
+        
 
 def kappa_scorer(y_true, y_pred):
     '''
