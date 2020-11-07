466d1d53627cf34633c5d4d7437cad4cd585417c
Marian Neagul <marian@info.uvt.ro>
2020-11-07 21:51:11 +0200

    Updating job submiter

--- tools/kb-job.py
+++ tools/kb-job.py
@@ -21,18 +21,54 @@ def submit_job(args):
     container_image = args.container
     container_name = args.name
 
+    body = client.V1Job(api_version="batch/v1", kind="Job", metadata=client.V1ObjectMeta(name=container_name))
+    body.status = client.V1JobStatus()
+    template = client.V1PodTemplate()
+
     labels = {
         'hugin-job': "1",
         'hugin-job-name': f'{container_name}'
     }
-
-    body = client.V1Job(api_version="batch/v1", kind="Job", metadata=client.V1ObjectMeta(name=container_name))
-    body.status = client.V1JobStatus()
-    template = client.V1PodTemplate()
-    template.template = client.V1PodTemplateSpec(metadata=client.V1ObjectMeta(labels=labels))
+    template.template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels=labels)
+    )
     env_list = []
-    container = client.V1Container(name=f"containter-{container_name}", image=container_image, env=env_list)
-    template.template.spec = client.V1PodSpec(containers=[container], restart_policy='Never', image_pull_secrets=[client.V1LocalObjectReference(name='dev-info-secret')])
+
+    tolerations = []
+    containe_args = dict(name=f"container-{container_name}", image=container_image, env=env_list)
+    if args.gpu:
+        tolerations.append(client.V1Toleration(
+        key='nvidia.com/gpu', operator='Exists', effect='NoSchedule'))
+        containe_args['resources'] = client.V1ResourceRequirements(limits={"nvidia.com/gpu": 1})
+
+    container = client.V1Container(**containe_args)
+    pull_secrets = []
+    if args.pull_secret is not None:
+        pull_secrets.append(client.V1LocalObjectReference(name=args.pull_secret))
+    pod_args = dict(containers=[container],
+                    restart_policy='Never',
+                    image_pull_secrets=pull_secrets)
+
+
+    if tolerations:
+        pod_args['tolerations'] = tolerations
+
+    if args.node_selector is not None:
+        parts = args.node_selector.split("=", 1)
+        if len(parts) == 2:
+            affinity = client.V1Affinity(
+                node_affinity=client.V1NodeAffinity(
+                    required_during_scheduling_ignored_during_execution=client.V1NodeSelector(
+                        node_selector_terms=[client.V1NodeSelectorTerm(
+                            match_expressions=[client.V1NodeSelectorRequirement(
+                                key=parts[0], operator='In', values=[parts[1]])]
+                        )]
+                    )
+                )
+            )
+            pod_args['affinity'] = affinity
+
+    template.template.spec = client.V1PodSpec(**pod_args)
     body.spec = client.V1JobSpec(ttl_seconds_after_finished=600, template=template.template)
     api_response = batch_v1.create_namespaced_job("default", body, pretty=True)
     print (api_response)
@@ -49,6 +85,10 @@ if __name__ == "__main__":
                               help='name of the jobs')
     submit_parser.add_argument('--container', type=str, required=True, default=None,
                                help='Id of the container')
+    submit_parser.add_argument('--pull-secret', type=str, required=False, default='dev-info-secret', help="The Kubernetes Pull Secret to be used")
+    submit_parser.add_argument("--gpu", action='store_true', default=False)
+    submit_parser.add_argument("--node-selector", type=str, default="sage-gpu=v100-dedicated", help="selector=value, eg: sage-gpu=v100-dedicated")
+    #submit_parser.add_argument()
     submit_parser.add_argument('--labels', type=str, required=False, default=None,
                                help='Id of the container')
     submit_parser.set_defaults(func=submit_job)
